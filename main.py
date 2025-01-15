8afc755aba0fde72392c6f7748985f9805f17058
LiLPandemio <markhaxx@gmail.com>
2025-01-15 20:55:34 +0100

    CCS-16 Main structure

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import json
+import base64
+import getpass
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidKey
+from pynput import keyboard
+import curses
+
+def main():
+    print("Welcome to the password manager!")
+    
+if __name__ == "__main__":
+    main()
